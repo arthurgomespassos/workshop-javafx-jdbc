9a0db56a3e648f77f6e168a986e51ff36e393adf
arthurgomespassos <arthurgomespassos74@gmail.com>
2022-07-10 12:30:59 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,9 @@
 module workshopJavafxJdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
-	
+
 	opens application to javafx.graphics, javafx.fxml, javafx.controls;
 	opens gui to javafx.graphics, javafx.fxml, javafx.controls;
+	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
+	opens model.services to javafx.graphics, javafx.fxml;
 }
